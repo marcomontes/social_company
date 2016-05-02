afde19d726fb02b3f6408140991d8a05711d2eee
Marco Montes <marcomontes@gmail.com>
2016-05-01 21:07:31 -0500

    Admin section with CRUDS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,4 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
-  end
-
 end
