fb4e9579912fac7c29e6a7f93af7320ed0001ad9
Marco Montes <marcomontes@gmail.com>
2016-05-04 00:22:51 -0500

    clean files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.destroy_all
 User.create( name: 'admin', email: 'admin@admin.com', password: 'admin123', password_confirmation: 'admin123', admin: true)
 user_1 = User.create( name: 'Marco Montes', email: 'marcomontes@gmail.com', password: 'marcomontes', password_confirmation: 'marcomontes')
@@ -15,8 +7,8 @@ user_4 = User.create( name: 'Pedro Botero', email: 'pedrobotero@gmail.com', pass
 
 
 Group.destroy_all
-Group.create(name: "Rol 1", user_ids: [user_1.id, user_2.id])
-Group.create(name: "Rol 2", user_ids: [user_1.id, user_3.id, user_4.id])
+Group.create(name: "Grupo 1", user_ids: [user_1.id, user_2.id])
+Group.create(name: "Grupo 2", user_ids: [user_1.id, user_3.id, user_4.id])
 
 Category.destroy_all
 Category.create(name: "Eventos")
